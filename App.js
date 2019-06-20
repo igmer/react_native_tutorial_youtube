caac3bd3fb5e9d5443cf0fda1679b873b40691b3
nirodriguez <nirodriguez@salud.gob.sv>
2019-06-20 10:40:42 -0600

    tutorial react native https://www.youtube.com/watch?v=xb8uTN3qiUI

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Main from './app/component/Main';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main/>
+    );
+  }
+}
+
+
